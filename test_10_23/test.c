ba6feb8af8a4c19bf31a50a576f4949f44978dff
gitee_com_zb <18749379836@163.com>
2022-10-23 21:46:02 +0800

    test_10_23

--- test_10_23/test.c
+++ test_10_23/test.c
@@ -3,17 +3,138 @@
 #include<stdio.h>
 int main()
 {
-	int i = 0;
-	int ch = 0;
-	while ((ch = getchar()) != EOF)
-	{
-		//putchar(ch+32);
-		//printf("\n");
-		printf("%c\n",ch+32);
-		getchar();
-	}
-	return 0;
+    char arr[] = { 73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116 , 33 ,0};
+    int i = 0;
+    int sz = strlen(arr);
+    for (i = 0; i < sz; i++)
+    {
+        printf("%c",arr[i]);
+    }
 }
+
+//int main()
+//{
+//    int i = 0;
+//    while ((scanf("%d", &i)) != EOF)
+//    {
+//        printf("%c", i);
+//    }
+//    return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	char ch = getchar();
+//	for (i = 0; i < 5; i++)
+//	{
+//		//先打印空格
+//		int j = 0;
+//		for (j = 0; j < 4-i; j++)
+//		{
+//			printf(" ");
+//		}
+//		//打印字符及其后面的空格
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf("%c ", ch);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//    char ch = 0;
+//    ch = getchar();
+//    printf("    %c\n", ch);
+//    printf("   %c %c\n", ch, ch);
+//    printf("  %c %c %c\n", ch, ch, ch);
+//    printf(" %c %c %c %c\n", ch, ch, ch, ch);
+//    printf("%c %c %c %c %c\n", ch, ch, ch, ch, ch);
+//    return 0;
+//}
+
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		getchar();
+//		printf("%d\n",ch);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//    int ch = 0;
+//    while ((ch = getchar()) != EOF)
+//    {
+//        if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
+//        {
+//            printf("YES\n");
+//        }
+//        else
+//        {
+//            printf("NO\n");
+//        }
+//        getchar();
+//    }
+//    return 0;
+//}
+//#include<ctype.h>
+//int main()
+//{
+//    int ch = 0;
+//    while ((ch = getchar()) != EOF)
+//    {
+//        if (isalpha(ch))
+//        {
+//            printf("YES\n");
+//        }
+//        else
+//        {
+//            printf("NO\n");
+//        }
+//        getchar();
+//    }
+//    return 0;
+//}
+//int main()
+//{
+//    int n = 0;
+//    float c_grade = 0;
+//    float math_grade = 0;
+//    float eng_grade = 0;
+//    scanf("%d;%f,%f,%f", &n, &c_grade, &math_grade, &eng_grade);
+//    printf("The each subject score of No. %d is %.2f, %.2f, %.2f.", n, c_grade, math_grade, eng_grade);
+//    return 0;
+//}
+
+//int main()
+//{
+//    int a = printf("Hello world!");
+//    printf("\n");
+//    printf("%d\n", a);
+//    return 0;
+//}
+//int main()
+//{
+//    printf("%15d\n", 0XABCDEF);
+//    return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		//putchar(ch+32);
+//		//printf("\n");
+//		printf("%c\n",ch+32);
+//		getchar();
+//	}
+//	return 0;
+//}
 //int main()
 //{
 //    char i = 0;
