26e9d8174f921a1d0dfa9932b00d62671fd5c025
gitee_com_zb <18749379836@163.com>
2022-11-13 19:46:45 +0800

    2022_11_13

--- test_11_13/test.c
+++ test_11_13/test.c
@@ -243,4 +243,43 @@
 //
 
 
+#include<stdio.h>
+
+void compare(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i <= sz - 1; i++)
+	{
+		int j = 0;
+		for (j = 0; j < sz -1-i; j++)
+		{
+			if (arr[j] < arr[j + 1])
+			{
+				int tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+}
+void print(int arr[],int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ",arr[i]);
+	}
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	scanf("%d %d %d", &a, &b, &c);
+	int arr[] = { a,b,c };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	compare(arr, sz);
+	print(arr,sz);
+	return 0;
+}
 
